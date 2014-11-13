73c470410f59777643bcc37e1b3d0582583f6bd3
bartbouten <bart.bouten@student.fontys.nl>
2014-11-13 12:55:00 +0100

    CharacterMovement start

--- Game/Assets/Scripts/CharacterMovement.cs
+++ Game/Assets/Scripts/CharacterMovement.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
+    CharacterController character;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            
+        }
 	}
 }
