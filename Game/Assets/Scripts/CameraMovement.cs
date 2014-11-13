8c5f7df8fab993de5ad79732e9ed5658ef18ac2f
Alexandervc <a.vancuijk@student.fontys.nl>
2014-11-13 21:27:03 +0100

    camera verbeterd

--- Game/Assets/Scripts/CameraMovement.cs
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -4,20 +4,21 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour {
     public Transform playerTransform;
 
-	private float middleWidth;
+	private float beginCam = 8.7f;
+	private float endCam = 18.5f;
 	private float camY = 5f;
 	private float camZ = -10f;
 
 	// Use this for initialization
 	void Start () 
 	{
-		middleWidth = 8.7f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (playerTransform.position.x >= middleWidth) 
+		if (playerTransform.position.x >= beginCam && playerTransform.position.x <) 
 		{
 			transform.position = new Vector3(playerTransform.position.x, camY, camZ);
 		}
