5b18684a3f24c4804fd23b0d209cbb63e3c485d9
xChocoMel <melaniekwetters@hotmail.com>
2014-11-13 13:25:19 +0100

    Images map

--- Game/Assets/Scripts/EnemyManager.cs
+++ Game/Assets/Scripts/EnemyManager.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
 
+	public enum Direction
+	{
+
+	}
+
+	public string name;
+	private int range;
+	private int speed;
+	private float position;
+	private 
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		//walkingrange of enemy
+		range = 10;
+		position = 0;
+
+		if (name.Equals("earth"))
+		{
+			speed = 1;
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		Move ();
+	}
+
+	private void Move ()
+	{
+		if(position <= range)
+		{
+
+		}
 	}
 }
