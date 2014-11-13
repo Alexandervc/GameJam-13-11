8d9c86c52583e4bcef3428393f938ab4439e8fc9
bartbouten <bart.bouten@student.fontys.nl>
2014-11-13 13:41:40 +0100

    CharacterMovement & InputDetection

---
+++ Game/Assets/Scripts/InputDetection.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+public class InputDetection : MonoBehaviour {
+	CharacterMovement character;
+
+	void Start() 
+	{
+		character = GetComponent<CharacterMovement> ();
+	}
+
+	void Update() 
+	{
+		//Touch Input
+		if(Input.touchCount == 1)
+		{
+			Touch touch = Input.touches[0];
+
+			Vector3 position = Camera.main.ScreenToWorldPoint(touch.position);
+
+			if(position.x > (Screen.width / 2))
+			{
+				character.MoveRight();
+			}
+			else
+			{
+				character.MoveLeft();
+			}
+		}
+
+
+		//Key Input
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+
+		}
+		
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			character.MoveLeft();
+		}
+		
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			
+		}
+		
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			character.MoveRight ();
+		}
+	}
+}
