c787da582080e3a9cb9bb944d0fd8212ad7351e2
Alexandervc <a.vancuijk@student.fontys.nl>
2014-11-13 12:54:43 +0100

    camera

--- Game/Assets/Scripts/CameraMovement.cs
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
+    public Transform playerTransform;
+
+    private Vector3 aimPos;
+    private float smooth;
 
 	// Use this for initialization
 	void Start () {
-	
+        smooth = 0.05f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        aimPos = playerTransform.position + (playerTransform.forward * -2.5f) + (playerTransform.up * 1.5f);
+        transform.position = Vector3.Lerp(transform.position, aimPos, smooth);
+        transform.LookAt(playerTransform);
 	}
 }
