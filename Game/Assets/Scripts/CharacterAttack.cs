9372e105a7802f5a5d682df16f267d86cab14f90
Alexandervc <a.vancuijk@student.fontys.nl>
2014-11-14 11:14:04 +0100

    attack

--- Game/Assets/Scripts/CharacterAttack.cs
+++ Game/Assets/Scripts/CharacterAttack.cs
@@ -3,28 +3,57 @@ using System.Collections;
 
 public class CharacterAttack : MonoBehaviour {
 	public GameObject projectilePrefab;
+	
+	private Transform attackingEnemy;
 	private Element playerElement;
+	private bool sucking = false;
+	private float suckTimer = 0f;
+	private float endOfSuck = 6f;
+
+	private Animator anim;
+	private int transformHash = Animator.StringToHash ("Transform");
+	private int suckHash = Animator.StringToHash("Suck");
 
 	// Use this for initialization
 	void Start () 
 	{
+		attackingEnemy = null;
 		playerElement = Element.normal;
+		anim = GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-	
+		if (sucking && attackingEnemy != null) 
+		{
+			suckTimer += Time.deltaTime;
+			if(suckTimer >= (endOfSuck / 2f))
+			{
+				// Destroy enemy
+				Destroy (attackingEnemy.gameObject);
+			}
+			if(suckTimer >= endOfSuck)
+			{
+				// Transform
+				anim.SetInteger ("Element", (int) playerElement);
+				anim.SetTrigger(transformHash);
+				attackingEnemy = null;
+				sucking = false;
+				suckTimer = 0f;
+			}
+		}
 	}
 
 	public void Attack(Transform enemyTransform)
 	{
 		Element enemyElement = enemyTransform.gameObject.GetComponent<EnemyManager>().GetElement();
-		if(Vector2.Distance((Vector2) this.transform.position, (Vector2) enemyTransform.position) < 0.5f)
+		if(Vector2.Distance((Vector2) this.transform.position, (Vector2) enemyTransform.position) < 1f)
 		{
-			// suck up
-			// play animation
+			attackingEnemy = enemyTransform;
 			this.playerElement = enemyElement;
+			this.sucking = true;
+			anim.SetTrigger (suckHash);
 		}
 		else
 		{
@@ -61,5 +90,10 @@ public class CharacterAttack : MonoBehaviour {
 	public Element GetElement()
 	{
 		return this.playerElement;
+	}
+
+	public bool GetSucking()
+	{
+		return sucking;
 	}
 }
